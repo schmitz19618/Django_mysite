765e567e38dfcf0a10b29ad6a6baca921578679a
brianschmitz <brian_schmitz@mac.com>
2014-02-20 19:09:23 -0800

    commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+TEMPLATE_DIRS = ('/Users/brianschmitz/PycharmProjects/Django_mysite/mysite/mysite/templates', )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
+    'south',
     'myblog',
 )
 
@@ -81,3 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,5 +8,15 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('myblog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('myblog.urls')), #<- already there
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {'template_name': 'login.html'},
+        name="login"),
+    url(r'^logout/$',
+        'django.contrib.auth.views.logout',
+        {'next_page': '/'},
+        name="logout"),
 )
